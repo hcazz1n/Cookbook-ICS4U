b003d288769fd94b7983f3d1ef78033d0691b801
kyleyjackson <kjackson@bayviewglen.ca>
2023-04-21 09:33:51 -0400

    idk what this commit is but vs code made me do it

--- back-end/server/db.js
+++ back-end/server/db.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connectionString = `mongodb+srv://fredhe56:ieatdogs69420@datacool.sduylk1.mongodb.net/cookbook?retryWrites=true&w=majority`
 
