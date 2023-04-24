80a6993634a099cefbeabe40c09783a86bbca9e7
DavidPerry06 <dperry@bayviewglen.ca>
2023-04-24 09:27:50 -0400

    Navbar component

--- front-end/vue-front/src/App.vue
+++ front-end/vue-front/src/App.vue
@@ -1,11 +1,11 @@
 <template>
+  <TheNavigation/>
   <div class="container has-text-centered">
-    <router-link to="/">Home</router-link>
-    <router-link to="/test">Test</router-link>
+
     <router-view></router-view>
   </div>
 </template>
 
 <script setup>
-
+ import TheNavigation from './components/TheNavigation.vue';
 </script>
--- front-end/vue-front/src/components/TheNavigation.vue
+++ front-end/vue-front/src/components/TheNavigation.vue
@@ -1,5 +1,10 @@
 <template>
-
+<div id="nav">
+    <router-link to="/">
+        <img src="../../public/placeholder.png" id="logo">
+    </router-link>
+    <router-link to="/" >Home</router-link>
+</div>
 </template>
 
 <script setup>
