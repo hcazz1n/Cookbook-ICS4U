5dbec7c00e8d738081b212b8c97705e126fecb90
Harrison Cazzin <hcazzin@bayviewglen.ca>
2023-04-25 17:08:08 -0400

    starting to style homepage

--- front-end/vue-front/src/App.vue
+++ front-end/vue-front/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <TheNavigation/>
   <div class="container has-text-centered">
-
     <router-view></router-view>
   </div>
 </template>
--- front-end/vue-front/src/views/HomeView.vue
+++ front-end/vue-front/src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <template>
-    <h1 class="is-size-1">Home</h1>
-    <button class="button has-background-primary-dark has-text-white-ter">Green</button>
+    <div id="homepage-container" class="container">
+        <h1 class="homepage-title has-text-white">COOKBOOK</h1>
+        <button class="button has-background-primary-dark has-text-white-ter">Green</button>
+    </div>
 </template>
 
 <script setup>
