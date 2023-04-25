12457780bf31f308af0140da2861e64e42efc852
Harrison Cazzin <hcazzin@bayviewglen.ca>
2023-04-25 19:22:27 -0400

    working on home page

--- front-end/vue-front/src/views/HomeView.vue
+++ front-end/vue-front/src/views/HomeView.vue
@@ -1,10 +1,29 @@
 <template>
     <div id="homepage-container" class="container">
         <h1 class="homepage-title has-text-white">COOKBOOK</h1>
-        <button class="button has-background-primary-dark has-text-white-ter">Green</button>
+        <div class="circle-container">
+            <div class="circle1"></div>
+            <div class="circle1"></div>
+            <div class="circle1"></div>
+        </div>
     </div>
 </template>
 
 <script setup>
-    
+    import {onMounted} from 'vue'
+
+    onMounted(() => {
+        let container = document.querySelector('circle-container')
+        let numCircles = screen.width / 5
+
+        let generatedCircles = 0
+
+        while(generatedCircles < numCircles){
+            let circle = document.createElement('div')
+            circle.classList.add('circle1')
+            container.appendChild('circle')
+            generatedCircles++
+        }
+    })
+
 </script>
