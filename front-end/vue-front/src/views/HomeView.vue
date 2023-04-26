1d2a4df352499a18607e8b45f92391f741087666
Harrison Cazzin <hcazzin@bayviewglen.ca>
2023-04-26 17:32:21 -0400

    fix

--- front-end/vue-front/src/views/HomeView.vue
+++ front-end/vue-front/src/views/HomeView.vue
@@ -1,60 +1,35 @@
 <template>
-    <div id="homepage-container">
+    <div id="homepage-container" class="container">
         <h1 class="homepage-title has-text-white">COOKBOOK</h1>
     </div>
-    <div class="circle-container"></div>
-    <div class="circle-container-2"></div>
-    <span class="circle-button-icon">
-        <i class="fa-solid fa-arrow-down has-text-danger"></i>
-    </span>
 </template>
 
 <script setup> 
-    import {onMounted} from 'vue'
-    
-        onMounted(() => {
-            let container = document.querySelector('.circle-container')
-            let generatedDistance = 0
-            while(generatedDistance < window.innerWidth / 2){
-                let circle = document.createElement('div')
-                circle.classList.add('circle1')
-                container.appendChild(circle)
-                generatedDistance += 45
-            }
-            container = document.querySelector('.circle-container-2')
-            generatedDistance = 0
-            while(generatedDistance < window.innerWidth / 2){
-                let circle = document.createElement('div')
-                circle.classList.add('circle1')
-                container.appendChild(circle)
-                generatedDistance += 45
-            }
-        })
-        
-        onMounted(() => {
-            let container = document.getElementById('homepage-container')
-            let circle = document.createElement('div')
-            circle.classList.add('circle-button')
-            container.appendChild(circle)
-        })
+    //import {onMounted} from 'vue'
 </script>
 
-<!-- <script setup>
-    import {onMounted} from 'vue'
+<!-- onMounted(() => {
+    //     let container = document.querySelector('.circle-container')
+    //     let generatedDistance = 0
+    //     while(generatedDistance < window.innerWidth / 2){
+    //         let circle = document.createElement('div')
+    //         circle.classList.add('circle1')
+    //         container.appendChild(circle)
+    //         generatedDistance += 45
+    //     }
+    //     container = document.querySelector('.circle-container-2')
+    //     generatedDistance = 0
+    //     while(generatedDistance < window.innerWidth / 2){
+    //         let circle = document.createElement('div')
+    //         circle.classList.add('circle1')
+    //         container.appendChild(circle)
+    //         generatedDistance += 45
+    //     }
+    // })
     
-    onMounted(() => {
-        let container = document.querySelector('.circle-container')
-        console.log(window.innerWidth)
-        let numCircles = window.innerWidth / 500
-
-        let generatedCircles = 0
-
-        while(generatedCircles < numCircles){
-            let circle = document.createElement('div')
-            circle.classList.add('circle1')
-            container.appendChild(circle)
-            generatedCircles++
-        }
-    })
-
-</script> -->
+    // onMounted(() => {
+    //     let container = document.getElementById('homepage-container')
+    //     let circle = document.createElement('div')
+    //     circle.classList.add('circle-button')
+    //     container.appendChild(circle)
+    // }) -->
