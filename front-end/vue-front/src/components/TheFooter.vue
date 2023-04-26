b72bbfd43379e8dbd5226b8bd6ed2bb316d2ef93
DavidPerry06 <dperry@bayviewglen.ca>
2023-04-26 09:49:29 -0400

    Footer

---
+++ front-end/vue-front/src/components/TheFooter.vue
@@ -0,0 +1,16 @@
+<template>
+
+<footer class="footer">
+  <div class="content has-text-centered">
+    <p>
+      <strong>COOKBOOK</strong>
+    </p>
+  </div>
+</footer>
+
+</template>
+
+<scrip setup>
+
+
+</scrip>
