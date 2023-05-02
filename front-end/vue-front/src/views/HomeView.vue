44ce452540d4c6705c6da8592b11f65781719164
Harrison Cazzin <hcazzin@bayviewglen.ca>
2023-05-01 22:55:18 -0400

    button animation adn style

--- front-end/vue-front/src/views/HomeView.vue
+++ front-end/vue-front/src/views/HomeView.vue
@@ -2,7 +2,7 @@
     <section id="home-title-hero" class="hero is-fullheight">
         <div class="homepage-container container">
             <h1 class="homepage-title has-text-white">COOKBOOK</h1>
-            <router-link to="#" class="hvr-float" v-scroll-to="{element: '#home-about-us', easing: [0.65, 0, 0.35, 1], duration: 2000}"><div class="button">Explore</div></router-link>
+            <router-link to="#" class="hvr-fade" v-scroll-to="{element: '#home-about-us', easing: [0.65, 0, 0.35, 1], duration: 2000}"><div class="button has-text-weight-light learn-more is-size-5">Learn More</div></router-link>
         </div>
     </section>
     <section id="home-about-us" class="hero is-fullheight">
