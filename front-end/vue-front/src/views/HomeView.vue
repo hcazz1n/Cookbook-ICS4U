7af7fe9b7e4521a63c0bf562183903abee7d63ac
Harrison Cazzin <hcazzin@bayviewglen.ca>
2023-05-04 18:46:41 -0400

    small changes to id

--- front-end/vue-front/src/views/HomeView.vue
+++ front-end/vue-front/src/views/HomeView.vue
@@ -5,9 +5,9 @@
             <router-link to="#" class="hvr-fade" v-scroll-to="{element: '#home-about-us', easing: [0.65, 0, 0.35, 1], duration: 2000}"><div class="button has-text-weight-light learn-more is-size-5">Learn More</div></router-link>
         </div>
     </section>
-    <section id="home-about-us" class="hero is-fullheight">
+    <section id="home-mission-statement" class="hero is-fullheight">
         <div class="homepage-container container">
-            <h1 class="homepage-title">Section 2</h1>
+            <h2 class="mission-title">Our Mission</h2>
         </div>
     </section>
 </template>
