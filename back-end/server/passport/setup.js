feba1c9ee569275e3856a03075df2392f70396e3
Fred He <fhe@bayviewglen.ca>
2023-05-08 09:45:28 -0400

    start of auth

---
+++ back-end/server/passport/setup.js
@@ -0,0 +1,54 @@
+const bcrypt = require('bcryptjs');
+const User = require('../models/Users');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy
+
+
+passport.serializeUser((user, done)=>{
+    done(null, user.id);
+})
+
+passport.deserializeUser((id, done)=>{
+    User.findById(id, (err, user)=>{
+        done(err, user);
+    })
+})
+
+passport.use(
+    new LocalStrategy({usernameField: "username"}, (username, password, done)=>{
+        User.findOne({username: username})
+        .then(user=>{
+            if(!user){
+                const newUser = new User({username, password})
+                bcrypt.genSalt(10, (err, salt)=>{
+                    bcrypt.hash(newUser.password, salt, (err, hash)=>{
+                        if(err) throw err;
+                        newUser.password = hash;
+                        newUser
+                        .save()
+                        .then(user=>{
+                            return done(null, user)
+                        })
+                        .catch(err=>{
+                            return done(null, false, {message: err})
+                        })
+                    })
+                })
+            }else{
+                bcrypt.compare(password, user.password, (err, isMatch)=>{
+                    if(err) throw err;
+                    if(isMatch){
+                        return done(null, user);
+                    }else{
+                        return done(null, false, {message: "Wrong Password"})
+                    }
+                })
+            }
+        })
+        .catch(err=>{
+            return done(null, false, {message: err})
+        })
+    })
+)
+
+module.exports = passport;
