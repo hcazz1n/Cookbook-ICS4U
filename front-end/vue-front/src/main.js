06e5339a81d6d7c880e1bbfb4546e848da5c8ed3
DavidPerry06 <dperry@bayviewglen.ca>
2023-05-10 09:46:27 -0400

    mounting font-awesome

--- front-end/vue-front/src/main.js
+++ front-end/vue-front/src/main.js
@@ -3,8 +3,13 @@ import './style.css'
 import App from './App.vue'
 import router from './router/index.js'
 import VueScrollTo from 'vue-scrollto'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+library.add(faUserSecret)
 
 createApp(App)
 .use(router)
 .use(VueScrollTo)
+.component('font-awesome-icon', FontAwesomeIcon)
 .mount('#app')
