52670315d2be823ecaa9d1d6772afb4f2263a02a
Harrison Cazzin <hcazzin@bayviewglen.ca>
2023-05-11 18:01:00 -0400

    beginning mission steatement

--- front-end/vue-front/src/views/HomeView.vue
+++ front-end/vue-front/src/views/HomeView.vue
@@ -2,16 +2,21 @@
     <section id="home-title-hero" class="hero is-fullheight">
         <div class="homepage-container container">
             <h1 class="homepage-title has-text-white">COOKBOOK</h1>
-            <router-link to="#" class="hvr-fade" v-scroll-to="{element: '#home-about-us', easing: [0.65, 0, 0.35, 1], duration: 2000}"><div class="button has-text-weight-light learn-more is-size-5">Learn More</div></router-link>
+            <router-link to="#" class="hvr-fade" v-scroll-to="{element: '#home-mission-statement', easing: [0.65, 0, 0.35, 1], duration: 2000}"><div class="button has-text-weight-light learn-more is-size-5">Learn More</div></router-link>
         </div>
     </section>
     <section id="home-mission-statement" class="hero is-fullheight">
-        <div class="homepage-container container">
+        <div class="homepage-container-2 container">
             <h2 class="mission-title">Our Mission</h2>
+            <div class="mission">
+                <p class="mp1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, esse quisquam quaerat repudiandae nostrum possimus deleniti ab fuga at dolore. Voluptatum, necessitatibus. Suscipit, consequuntur voluptates quibusdam saepe vel tempora, porro quo blanditiis esse officiis perferendis nihil quidem odit laboriosam? Adipisci ex aspernatur cupiditate omnis voluptas quibusdam? Nam esse omnis saepe animi odit eum cupiditate, vel doloribus quod architecto asperiores, tempora quis laboriosam sapiente.</p>
+                <p class="p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, esse quisquam quaerat repudiandae nostrum possimus deleniti ab fuga at dolore. Voluptatum, necessitatibus. Suscipit, consequuntur voluptates quibusdam saepe vel tempora, porro quo blanditiis esse officiis perferendis nihil quidem odit laboriosam? Adipisci ex aspernatur cupiditate omnis voluptas quibusdam? Nam esse omnis saepe animi odit eum cupiditate, vel doloribus quod architecto asperiores, tempora quis laboriosam sapiente.</p>
+                <p class="p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, esse quisquam quaerat repudiandae nostrum possimus deleniti ab fuga at dolore. Voluptatum, necessitatibus. Suscipit, consequuntur voluptates quibusdam saepe vel tempora, porro quo blanditiis esse officiis perferendis nihil quidem odit laboriosam? Adipisci ex aspernatur cupiditate omnis voluptas quibusdam? Nam esse omnis saepe animi odit eum cupiditate, vel doloribus quod architecto asperiores, tempora quis laboriosam sapiente.</p>
+            </div>
         </div>
     </section>
 </template>
 
 <script setup> 
-    //import {onMounted} from 'vue'
+    
 </script>
