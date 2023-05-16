7bae6fdc89804b3751636ac40eca524bb12267c2
DavidPerry06 <dperry@bayviewglen.ca>
2023-05-16 09:11:25 -0400

    RecipeS

--- front-end/vue-front/src/router/index.js
+++ front-end/vue-front/src/router/index.js
@@ -4,7 +4,7 @@ const routes = [
     {path: '/', name: 'Home', component: () => import('../views/HomeView.vue')},
     {path: '/account', name: 'Account', component: () => import('../views/AccountView.vue')},
     {path: '/contact', name: 'Contact Us', component: () => import('../views/ContactView.vue')},
-    {path: '/recipe', name: 'Recipe', component: () => import('../views/RecipeView.vue')},
+    {path: '/recipes', name: 'Recipes', component: () => import('../views/RecipeView.vue')},
     {path: '/login', name: 'Login', component: () => import('../views/LoginView.vue')},
     {path: '/test', name: 'Test', component: () => import('../views/TestView.vue')},
 ]
