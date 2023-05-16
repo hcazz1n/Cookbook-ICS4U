5450c12f9f45fc5726a667d12f2c1037c5246673
Fred He <fhe@bayviewglen.ca>
2023-05-16 09:50:28 -0400

    auth better

--- back-end/server/passport/setup.js
+++ back-end/server/passport/setup.js
@@ -1,54 +1,48 @@
-const bcrypt = require('bcryptjs');
-const User = require('../models/Users');
-const passport = require('passport');
+const bcrypt = require('bcryptjs')
+const User = require('../models/Users')
+const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 
-
-passport.serializeUser((user, done)=>{
-    done(null, user.id);
+passport.serializeUser((user, done) => {
+  console.log(2)
+  done(null, user.id)
 })
 
-passport.deserializeUser((id, done)=>{
-    User.findById(id, (err, user)=>{
-        done(err, user);
-    })
+passport.deserializeUser((user, cb) => {
+  // console.log(1)
+  // console.log(id)
+  // console.log(1)
+
+  // User.findById(id).then(function(err, user){
+  //   done(err, user)
+  // })
+  process.nextTick(function() {
+    return cb(null, user);
+  });
+  
 })
 
 passport.use(
-    new LocalStrategy({usernameField: "username"}, (username, password, done)=>{
-        User.findOne({username: username})
-        .then(user=>{
-            if(!user){
-                const newUser = new User({username, password})
-                bcrypt.genSalt(10, (err, salt)=>{
-                    bcrypt.hash(newUser.password, salt, (err, hash)=>{
-                        if(err) throw err;
-                        newUser.password = hash;
-                        newUser
-                        .save()
-                        .then(user=>{
-                            return done(null, user)
-                        })
-                        .catch(err=>{
-                            return done(null, false, {message: err})
-                        })
-                    })
-                })
-            }else{
-                bcrypt.compare(password, user.password, (err, isMatch)=>{
-                    if(err) throw err;
-                    if(isMatch){
-                        return done(null, user);
-                    }else{
-                        return done(null, false, {message: "Wrong Password"})
-                    }
-                })
-            }
+  new LocalStrategy(
+    { usernameField: 'name' },
+    (name, password, done) => {
+      console.log(3)
+      User.findOne({ name: name })
+        .then((user) => {
+            bcrypt.compare(password, user.password, (err, isMatch) => {
+              if (err) throw err
+              if (isMatch) {
+                return done(null, user)
+              } else {
+                return done(null, false, { message: 'Wrong Password' })
+              }
+            })
         })
-        .catch(err=>{
-            return done(null, false, {message: err})
+        .catch((err) => {
+          return done(null, false, { message: err })
         })
-    })
+    }
+  )
 )
 
-module.exports = passport;
+module.exports = passport
