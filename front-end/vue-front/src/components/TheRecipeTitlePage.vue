d7c6ae0e9fd48044c291e4a9b4c7382ab2ce9497
Harrison Cazzin <hcazzin@bayviewglen.ca>
2023-05-30 09:18:05 -0400

    more changes

---
+++ front-end/vue-front/src/components/TheRecipeTitlePage.vue
@@ -0,0 +1,8 @@
+<template>
+    <img class="recipe-img" src="macaroncoffeescaled.jpg">
+    <div class="recipe-title is-size-1 has-text-black">[Recipe Name]</div>
+</template>
+
+<script setup>
+
+</script>
