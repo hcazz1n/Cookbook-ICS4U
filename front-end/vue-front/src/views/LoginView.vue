ff40e7277018689a583dfa0f9260550308dd8aaf
Fred He <fhe@bayviewglen.ca>
2023-05-30 14:32:18 -0400

    post

--- front-end/vue-front/src/views/LoginView.vue
+++ front-end/vue-front/src/views/LoginView.vue
@@ -1,40 +1,81 @@
 <template>
-    <section class="hero is-fullheight">
-            <div class="hero-body is-justify-content-center is-align-items-center">
-                <div class="columns is-flex is-flex-direction-column box" @submit.prevent="login">
-                    <div class="column">
-                        <label for="email">Username</label>
-                        <input class="input is-danger" type="text" placeholder="Username">
-                    </div>
-                    <div class="column">
-                        <label for="Name">Password</label>
-                        <input class="input is-danger" type="password" placeholder="Password">
-                        <!-- a href="forget.html" class="is-size-7 has-text-primary"></a> -->
-                    </div>
-                    <div class="column">
-                        <button class="button is-danger is-fullwidth" type="submit">Login</button>
-                    </div>
-                    <div class="has-text-centered">
-                        <p class="is-size-7"> Don't have an account? 
-                            <router-link to="register" class="has-text-danger">Register</router-link>
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </section>
+  <section class="hero is-fullheight">
+    <div class="hero-body is-justify-content-center is-align-items-center">
+      <div
+        class="columns is-flex is-flex-direction-column box"
+        @submit.prevent="login"
+      >
+        <div class="column">
+          <label for="email">Username</label>
+          <input
+            class="input is-danger"
+            type="text"
+            placeholder="Username"
+            name="Username"
+          />
+        </div>
+        <div class="column">
+          <label for="Name">Password</label>
+          <input
+            class="input is-danger"
+            type="password"
+            placeholder="Password"
+            name="password"
+          />
+          <!-- a href="forget.html" class="is-size-7 has-text-primary"></a> -->
+        </div>
+        <div class="column">
+          <button
+            class="button is-danger is-fullwidth"
+            type="submit"
+            @click="login()"
+          >
+            Login
+          </button>
+        </div>
+        <div class="has-text-centered">
+          <p class="is-size-7">
+            Don't have an account?
+            <router-link to="register" class="has-text-danger"
+              >Register</router-link
+            >
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
-<script setup>
-    import {ref} from 'vue'
-    import {useRouter, useRoute} from 'vue-router'
+<script>
+import axios from 'axios'
+import { ref } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
 
-    const username = ref('')
-    const password = ref('')
-    const router = useRouter()
-    const route = useRoute()
-    const login = () => {
-        window.user = username.value
-        const redirectPath = route.query.redirect || '/account'
-        router.push(redirectPath)
-    }
+const username = ref('')
+const password = ref('')
+const router = useRouter()
+const route = useRoute()
+const login = () => {
+  window.user = username.value
+  const redirectPath = route.query.redirect || '/account'
+  router.push(redirectPath)
+}
+
+export default {
+  methods: {
+    login() {
+      axios
+        .post('http://localhost:3000/login', (req,res,next)=>{
+            console.log("hello")
+        })
+        .then((response) => {
+          this.data = response.data
+          console.log(this.data)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+  },
+}
 </script>
